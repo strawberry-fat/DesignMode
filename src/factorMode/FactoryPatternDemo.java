eecc8ee267da7362aae9aa54046a9b5ce055f252
Mr.YL <13037627713@163.com>
2019-12-03 09:57:06 +0800

    设计模式之工厂模式

--- src/factorMode/FactoryPatternDemo.java
+++ src/factorMode/FactoryPatternDemo.java
@@ -1,5 +1,11 @@
 package factorMode;
 
+/**
+ * 我们将创建一个 Shape 接口和实现 Shape 接口的实体类。下一步是定义工厂类 ShapeFactory。
+ *
+ * FactoryPatternDemo，我们的演示类使用 ShapeFactory 来获取 Shape 对象。
+ * 它将向 ShapeFactory 传递信息（CIRCLE / RECTANGLE / SQUARE），以便获取它所需对象的类型。
+ */
 public class FactoryPatternDemo {
  
    public static void main(String[] args) {
